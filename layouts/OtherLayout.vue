4232beb0f5ec95ad409e16a270d05321394c11c9
Jesse <jessemerge2@gmail.com>
2023-08-22 16:45:32 -0700

    Create pages/components/layouts/& global css

---
+++ layouts/OtherLayout.vue
@@ -0,0 +1,9 @@
+<template>
+	<div>
+		<p>This is a main layout. Testing if you can see pages, layouts, and buttons below</p>
+		<StandardButton>Standard</StandardButton>
+		<OutlineButton> Outline</OutlineButton>
+	</div>
+</template>
+
+<script setup></script>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+	THis is an index
+	<OtherLayout />
+</template>
+
+<script setup>
+	import OtherLayout from '~/layouts/OtherLayout.vue';
+
+	// onBeforeMount(async () => {
+	// 	try {
+	// 		isLoading.value = true;
+	// 		await somefunction()
+	// 		isLoading.value = false;
+	// 	} catch (error) {
+	// 		console.log(error);
+	// 	}
+	// });
+</script>

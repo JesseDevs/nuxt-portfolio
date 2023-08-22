a68c1ea6d94bc8794d435e94ad97abeda4f4f507
Jesse <jessemerge2@gmail.com>
2023-08-22 16:45:52 -0700

    Install and change configurations

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+	pages: true,
+	devtools: { enabled: true },
+	vue: {
+		compilerOptions: {
+			// treat all tags with a dash as custom elements
+			isCustomElement: (tag) => tag.includes('-'),
+		},
+	},
+	app: {
+		head: {
+			title: 'Nuxt Portfolio',
+			meta: [
+				{ charset: 'utf-8' },
+				{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			],
+			link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+		},
+	},
+	css: ['~/assets/css/globals.css'],
+	components: true,
+});

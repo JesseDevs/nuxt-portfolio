794596f09d8a6cea8f593e494700e1276a807208
Jesse <jessemerge2@gmail.com>
2023-08-29 12:05:50 -0700

    add details to contact

--- components/modules/ContactPage.vue
+++ components/modules/ContactPage.vue
@@ -1,19 +1,19 @@
 <template>
 	<contact-page class="grid-template">
 		<text-content>
-			<SectionHeader :title="moduleData.heading" dir="r" />
+			<SectionHeader :title="moduleData.heading" dir="rr" />
 
-			<p class="intro">{{ moduleData.intro }}</p>
+			<h5 class="strict-voice">{{ moduleData.intro }}</h5>
 
 			<p v-html="moduleData.text"></p>
 		</text-content>
-		<!--
-		<Link href="mailto:bob.ross@notreal.com">
-			<div className="{styles.contactEmail}">
-				<AiFillMail size="2.4rem" />
-				<span>bob.ross@notreal.com</span>
-			</div>
-		</Link> -->
+
+		<div class="contactEmail">
+			<a href="jessemerge2@gmail.com">
+				<Icon name="material-symbols:mail" size="2.4rem" />
+				<span class="strict-voice">jessemerge2@gmail.com</span>
+			</a>
+		</div>
 	</contact-page>
 </template>
 
@@ -22,3 +22,30 @@
 		moduleData: Object,
 	});
 </script>
+
+<style lang="scss">
+	.contactEmail {
+		a {
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			gap: 0.8rem;
+			width: fit-content;
+			font-size: var(--text-md);
+			margin: auto;
+			padding-top: 2rem;
+			transition: 0.25s color;
+		}
+	}
+
+	.contactEmail:hover {
+		color: var(--brand);
+	}
+
+	contact-page {
+		p a {
+			color: var(--brand);
+			text-decoration: underline;
+		}
+	}
+</style>

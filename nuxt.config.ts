fd581c9e99b935815ce27c2cfb05f234e8a1aca3
Jesse <jessemerge2@gmail.com>
2023-09-16 19:08:35 -0700

    something with meta description

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,15 +18,12 @@ export default defineNuxtConfig({
 				{ charset: 'utf-8' },
 				{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
 				{
-					hid: 'description',
 					name: 'description',
-					property: 'description',
 					content:
 						'Welcome to my personal site! Take a look around and find something to peak your interest.. or mine.',
 				},
 				{
 					hid: 'og:description',
-					name: 'og:description',
 					property: 'og:description',
 					content:
 						'Welcome to my personal site! Take a look around and find something to peak your interest.. or mine.',

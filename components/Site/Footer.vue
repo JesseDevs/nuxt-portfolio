811a088084179df3aa1471c1f918985cbc7b44f9
Jesse <jessemerge2@gmail.com>
2023-09-29 01:58:46 -0700

    Add footer

---
+++ components/Site/Footer.vue
@@ -0,0 +1,155 @@
+<template>
+	<site-footer class="small-voice">
+		<inner-column>
+			<nav>
+				<ul class="nav-container page">
+					<h3>Page Routes</h3>
+
+					<li>
+						<NuxtLink to="/" @click="ui.closeMenu">
+							<span>Home</span>
+						</NuxtLink>
+					</li>
+					<li>
+						<a href="/#ProjectSection" @click="ui.closeMenu">
+							<span>Projects</span>
+						</a>
+					</li>
+					<li>
+						<a href="/#AboutCard" @click="ui.closeMenu">
+							<span>About</span>
+						</a>
+					</li>
+					<li>
+						<NuxtLink to="/writing" @click="ui.closeMenu">
+							<span>Writing</span>
+						</NuxtLink>
+					</li>
+
+					<li>
+						<a href="#ContactPage" @click="ui.closeMenu">
+							<span>Contact</span>
+						</a>
+					</li>
+				</ul>
+
+				<ul class="socials">
+					<h3>Socials</h3>
+					<li><a href="https://github.com/JesseDevs" target="_blank"> Github </a></li>
+					<li>
+						<a href="https://www.linkedin.com/in/felix-jesser/" target="_blank">
+							LinkedIn
+						</a>
+					</li>
+					<li><a href="https://twitter.com/Jesse_Dude" target="_blank"> Twitter </a></li>
+					<li>
+						<a href="https://www.instagram.com/jessedude2/" target="_blank"
+							>Instagram</a
+						>
+					</li>
+					<li><a href="/webdev-resume-jesse.pdf" target="_blank"> Resume </a></li>
+				</ul>
+				<div class="footnote">
+					<div class="frame-con">
+						<iframe
+							allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write"
+							frameborder="0"
+							style="
+								width: 100%;
+								max-width: 300px;
+								overflow: hidden;
+								border-radius: 10px;
+								background-color: black;
+							"
+							sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation"
+							src="https://embed.music.apple.com/us/album/ferxxo-151/1704049454?i=1704050427"
+						></iframe>
+					</div>
+					<p>© 2023 • Jesse Felix</p>
+				</div>
+			</nav>
+		</inner-column>
+	</site-footer>
+</template>
+
+<script setup>
+	import { useInterfaceStore } from '~/stores/interface';
+
+	const ui = useInterfaceStore();
+</script>
+
+<style lang="scss" scoped>
+	site-footer {
+		display: block;
+		width: 100%;
+		position: relative;
+		background: linear-gradient(180deg, var(--background), rgb(var(--brand-rgb) / 0.2));
+		backdrop-filter: brightness(0.7);
+
+		* {
+			font-size: inherit;
+		}
+
+		nav {
+			border-top: 1px solid rgba(246, 244, 244, 0.301);
+			padding-top: 2rem;
+			display: flex;
+			flex-direction: column;
+			gap: 4rem;
+
+			h3 {
+				font-size: var(--step-0);
+				text-decoration: underline;
+				font-weight: 500;
+			}
+
+			ul {
+				@media (min-width: 500px) {
+					padding: 0 5rem;
+				}
+				display: flex;
+				flex-wrap: wrap;
+				column-gap: 1rem;
+				row-gap: 1rem;
+
+				h3 {
+					flex-grow: 1;
+					flex-shrink: 0;
+					flex-basis: 100%;
+				}
+				li {
+					flex-grow: 1;
+					text-align: center;
+
+					a {
+						padding: 10px;
+						font-weight: 400;
+						display: block;
+						transition: background-color 0.2s ease-in-out, opacity 0.2s ease-in-out;
+						border-radius: 0.375rem;
+						opacity: 0.85;
+						color: var(--white);
+
+						&:hover {
+							background-color: rgb(var(--black-rgb) / 0.7);
+							opacity: 1;
+						}
+					}
+				}
+			}
+		}
+
+		.footnote {
+			display: flex;
+			justify-content: space-between;
+			align-items: flex-end;
+			flex-wrap: wrap;
+			column-gap: 5rem;
+			row-gap: 2rem;
+
+			p {
+				flex-shrink: 0;
+			}
+		}
+	}
+</style>
---
+++ components/Site/Nav.vue
@@ -0,0 +1,102 @@
+<template>
+	<header class="main-tool-bar site-header">
+		<inner-column>
+			<nav class="main-menu">
+				<MyLinks />
+				<button class="hamburger-button icon-button" @click="ui.toggleMainMenu">
+					<Icon name="grommet-icons:menu" size="21" color="var(--text)" />
+				</button>
+
+				<NavContainer class="big-screen-nav" />
+			</nav>
+		</inner-column>
+
+		<MenuModal />
+	</header>
+</template>
+
+<script setup>
+	import { useInterfaceStore } from '~/stores/interface';
+
+	const ui = useInterfaceStore();
+</script>
+
+<style lang="scss">
+	header {
+		// position: fixed;
+		// top: 0;
+		width: 100%;
+		z-index: 20;
+		background-color: var(--background);
+		box-shadow: 0px 0px 5px rgba(128, 128, 128, 0.5);
+
+		inner-column {
+			padding: 1.5rem 2rem;
+
+			.main-menu {
+				display: flex;
+				flex-direction: row;
+				align-items: center;
+				justify-content: space-between;
+
+				.icon-button {
+					&:hover {
+						background-color: rgba(166, 163, 163, 0.4);
+					}
+				}
+			}
+		}
+	}
+
+	.icon-button {
+		outline: none;
+		border: none;
+		background-color: transparent;
+		border-radius: 4px;
+		padding: 6px;
+		transition: background-color 0.2s ease-in-out;
+		cursor: pointer;
+
+		&:hover {
+			background-color: var(--fade);
+		}
+	}
+
+	nav.main-menu {
+		ul.big-screen-nav {
+			flex-direction: row;
+			display: none;
+			opacity: 0;
+			pointer-events: none;
+			transition: opacity 0.2s ease;
+			padding: 0;
+			gap: 0.5rem;
+
+			a {
+				font-size: var(--step--1);
+				font-weight: 300;
+				color: var(--text);
+
+				svg {
+					display: none;
+				}
+			}
+
+			.big-theme-toggle {
+				display: block;
+			}
+		}
+
+		@media (min-width: 900px) {
+			ul.big-screen-nav {
+				display: flex;
+				opacity: 1;
+				pointer-events: all;
+			}
+
+			.hamburger-button {
+				display: none;
+			}
+		}
+	}
+</style>
